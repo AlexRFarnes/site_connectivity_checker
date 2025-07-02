b1aacc222d246f33190ea4cc627fbe64e5e4d4e6
AlexRFarnes <alexrfarnes@gmail.com>
2025-07-03 05:31:48 +0800

    implement the functionality to check a site's connectivity

--- scchecker/checker.py
+++ scchecker/checker.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from site-connectivity-checker!")
+# checker.py
+
+from http.client import HTTPConnection
+from urllib.parse import urlparse
 
 
-if __name__ == "__main__":
-    main()
+def site_is_online(url, timeout=2):
+    """Return True if the target URL is online,
+    Raise an exception otherwise."""
+    error = Exception("unknown error")
+    parser = urlparse(url)
+    host = parser.netloc or parser.path.split("/")[0]
+    for port in (80, 443):
+        connection = HTTPConnection(host=host, port=port, timeout=timeout)
+        try:
+            connection.request("HEAD", "/")
+            return True
+        except Exception as e:
+            error = e
+        finally:
+            connection.close()
+    raise error
